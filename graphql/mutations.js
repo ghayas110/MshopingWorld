a1c605da5569286d9c9df62dabe532524b0306fc
Talha-Shekhani <60299561+Talha-Shekhani@users.noreply.github.com>
2021-03-24 13:10:31 +0500

    T_amplify

---
+++ graphql/mutations.js
@@ -0,0 +1,135 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createUser = /* GraphQL */ `
+  mutation CreateUser(
+    $input: CreateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    createUser(input: $input, condition: $condition) {
+      id
+      username
+      email
+      phone_number
+      category
+      parentId
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateUser = /* GraphQL */ `
+  mutation UpdateUser(
+    $input: UpdateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    updateUser(input: $input, condition: $condition) {
+      id
+      username
+      email
+      phone_number
+      category
+      parentId
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteUser = /* GraphQL */ `
+  mutation DeleteUser(
+    $input: DeleteUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    deleteUser(input: $input, condition: $condition) {
+      id
+      username
+      email
+      phone_number
+      category
+      parentId
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createProduct = /* GraphQL */ `
+  mutation CreateProduct(
+    $input: CreateProductInput!
+    $condition: ModelProductConditionInput
+  ) {
+    createProduct(input: $input, condition: $condition) {
+      id
+      userId
+      user {
+        id
+        username
+        email
+        phone_number
+        category
+        parentId
+        createdAt
+        updatedAt
+      }
+      title
+      description
+      price
+      quantity
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateProduct = /* GraphQL */ `
+  mutation UpdateProduct(
+    $input: UpdateProductInput!
+    $condition: ModelProductConditionInput
+  ) {
+    updateProduct(input: $input, condition: $condition) {
+      id
+      userId
+      user {
+        id
+        username
+        email
+        phone_number
+        category
+        parentId
+        createdAt
+        updatedAt
+      }
+      title
+      description
+      price
+      quantity
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteProduct = /* GraphQL */ `
+  mutation DeleteProduct(
+    $input: DeleteProductInput!
+    $condition: ModelProductConditionInput
+  ) {
+    deleteProduct(input: $input, condition: $condition) {
+      id
+      userId
+      user {
+        id
+        username
+        email
+        phone_number
+        category
+        parentId
+        createdAt
+        updatedAt
+      }
+      title
+      description
+      price
+      quantity
+      createdAt
+      updatedAt
+    }
+  }
+`;
